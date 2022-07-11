95bea081df91e3b5c295e420a31db28e5f8181a6
Riley Hoffman <rileyjhoff@gmail.com>
2022-07-11 11:00:40 -0700

    change test descriptions to be more accurate

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const { push, pop, unshift, shift } = require('./index.js');
 
 describe('push', () => {
-  it('should add an element to an array', () => {
+  it('should add an element to the end of an array', () => {
     const arr = ['a', 'b', 'c'];
     expect(push(arr, 'd')).toBe(4);
     expect(arr.length).toBe(4);
@@ -10,7 +10,7 @@ describe('push', () => {
 });
 
 describe('pop', () => {
-  it('should remove an element from an array', () => {
+  it('should remove the last element from an array', () => {
     const arr = ['a', 'b', 'c'];
     expect(pop(arr)).toBe('c');
     expect(arr.length).toBe(2);
@@ -19,7 +19,7 @@ describe('pop', () => {
 });
 
 describe('unshift', () => {
-  it('should add an element to an array', () => {
+  it('should add an element to the front of an array', () => {
     const arr = ['a', 'b', 'c'];
     expect(unshift(arr, 'd')).toBe(4);
     expect(arr.length).toBe(4);
